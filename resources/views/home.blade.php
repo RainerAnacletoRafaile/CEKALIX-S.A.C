9d59472f4ef854c85b0feace1b002da0b266b316
StartMakers <raineranacleto8@gmail.com>
2026-09-26 06:31:10 -0500

    Agrega Loggin funcionando

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Panel de bienvenida al inventario</h1>
+    <p>Bienvenido {{auth()->user()->name}}</p>
+</body>
+</html>
